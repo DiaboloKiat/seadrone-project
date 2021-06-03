c005b3b9d2ea8e18eb0ea621365244d7727754d4
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-03 15:21:43 +0800

    06_03

--- github/git-push-nano.sh
+++ github/git-push-nano.sh
@@ -4,9 +4,10 @@ git config --global user.name "DiaboloKiat"
 git config --global user.email "DiaboloKiat@gmail.com"
 
 git status
+git checkout devel-nano
 echo "Enter your message"
 read message
-BRANCH=master
+BRANCH=devel-nano
 
 
 # push main
