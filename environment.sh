b3f33d8235c45caf4aba270cc5629fc826241d1d
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-04 20:49:18 +0800

    06_04

--- environment.sh
+++ environment.sh
@@ -1,7 +1,3 @@
 source ~/seadrone-project/catkin_ws/devel/setup.bash
 
-<<<<<<< HEAD
-source ~/seadrone-project/catkin_ws/install/setup.bash --extend
-=======
 # source ~/seadrone-project/catkin_ws/install/setup.bash --extend
->>>>>>> e0a64391c69706daa8dd4bf4937af70de0032217
