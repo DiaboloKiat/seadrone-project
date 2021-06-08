385d30029df81763ce43c7a81110ff47d2b491b9
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-08 12:40:06 +0800

    06_08

--- catkin_make.sh
+++ catkin_make.sh
@@ -1,2 +1,8 @@
-catkin_make -C ~/seadrone-project/catkin_ws
-cd ~/seadrone-project
+catkin_make --pkg vision_opencv -C ./catkin_ws \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+    -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
+    -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+
+
+catkin_make -C ./catkin_ws
--- environment.sh
+++ environment.sh
@@ -1,3 +1,4 @@
 source ~/seadrone-project/catkin_ws/devel/setup.bash
+source /opt/ros/melodic/setup.bash --extend
 
-source ~/seadrone-project/catkin_ws/install/setup.bash --extend
+# source ~/seadrone-project/catkin_ws/install/setup.bash --extend
