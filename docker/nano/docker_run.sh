252925b3201d4232695b8af443ef8ca95a6c52de
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-08 16:01:55 +0800

    06_08

--- docker/nano/docker_run.sh
+++ docker/nano/docker_run.sh
@@ -1,4 +1,22 @@
  
 #!/bin/bash
 
-docker run -it --rm --runtime nvidia diabolokiat/seadrone-project:nano
+docker run -it \
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
+  -v "/home/$USER/seadrone-project:/home/seadrone/seadrone-project" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+  -v "/etc/localtime:/etc/localtime:ro" \
+  -v "/dev:/dev" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  -v "/home/$USER/.bashrc:/home/seadrone/.bashrc" \
+  --name seadrone-project \
+  --network host \
+  --rm \
+  $DOCKER_OPTS \
+  --privileged \
+  --security-opt seccomp=unconfined \
+  diabolokiat/seadrone-project:nano \
+  bash
